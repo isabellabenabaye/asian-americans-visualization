df0a075e27989d5395e2d055d12b9cdab66614ca
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-05-14 19:59:13 -0700

    :art: almost final edit? new colors, better alignment

--- data_viz.R
+++ data_viz.R
@@ -11,15 +11,16 @@ loadfonts(device = "win", quiet = TRUE)
 theme_set(theme_minimal())
 theme <- theme_update(text = element_text(family = "Karla", size = 13),
                       title = element_text("Karla", size = 24, color = "gray20"),
-                      plot.title = element_text("Karla", face = "bold", size = 24, color = "gray20"),
-                      plot.title.position = "plot",
+                      plot.title = element_text("Karla", face = "bold", size = 26, color = "gray50", hjust = 0.05),
+                      #plot.title.position = "plot",
                       plot.subtitle = element_text("Inter",size = 26, color = "gray20"),
                       panel.grid = element_blank(),
                       axis.text = element_text(size = 14),
                       axis.text.x = element_blank(),
                       axis.line.x = element_blank(),
                       axis.line.y = element_line(color = "gray80"),
-                      plot.margin = margin(15, 25, 5,0 ))
+                      plot.margin = margin(35, 30, 5,5 ),
+                      plot.background = element_rect(fill = "#F3F4F6", color = "#F3F4F6"))
 
 # Source: https://data.census.gov/cedsci/table?g=0100043US_0400000US06&hidePreview=false&tid=ACSDT1Y2018.B02018&table=DP05&vintage=2018&cid=HCT011001&t=Asian&layer=VT_2010_040_00_PY_D1
 asians_pop <- readxl::read_xlsx("data/race_data.xlsx",col_names = c("race","population"),
@@ -41,7 +42,7 @@ islander_pop <- readxl::read_xlsx("data/race_islander_data.xlsx",col_names = c("
 pob <- readxl::read_xlsx("data/place_of_birth_citizenship.xlsx",col_names = c("place_of_birth","population"),
                                   range = "A2:B11") %>% 
   filter(!(place_of_birth %in% c("Male","Female",	"Foreign born"))) %>% 
-  mutate(label = "Place of Birth") %>% 
+  mutate(label = 1) %>% 
   mutate(pct = prop.table(population))
 
 entered <- readxl::read_xlsx("data/place_of_birth_citizenship.xlsx",col_names = c("entered","pct"),
@@ -71,43 +72,65 @@ pop_plot <- pop_data %>%
   geom_text(aes(race,pop, label = scales::comma(pop)),nudge_y = 50000, family = "Inconsolata", size = 4.5, hjust = 0) +
   labs(
     #title = "Asian and Pacific Islander American Heritage Month", 
-    title = "Asian and Pacific Islander Populations", x = "", y = "") +
+    title = "<span style = 'color:#18377A;'>Asian</span> and <span style = 'color:#55DDE0;'>Pacific Islander</span> Populations in the United States", x = "", y = "") +
   scale_y_continuous(expand = expansion(0,0),labels = scales::comma, limits = c(0,5600000)) +
-  scale_fill_paletteer_d("rcartocolor::Pastel") +
-  coord_flip()
+  scale_fill_manual(values = c("#18377A","#55DDE0")) +
+  #scale_fill_paletteer_d("RColorBrewer::Set2") +
+  coord_flip() +
+  theme(plot.title = element_markdown(),
+        plot.title.position = "plot")
 
-pob_plot <- pob %>% 
+pob_plot <- 
+  pob %>% 
   ggplot(aes(label, population, fill = fct_reorder(place_of_birth,population))) +
   geom_col(show.legend = FALSE) +
-  labs(title = "Of the 22.1 million Asians in the US, 57.1% are foreign born",x = "", y = "") + 
-  geom_text(aes(label = scales::percent(pct)), 
-            position = position_stack(vjust = 0.5), family = "Karla", size = 4) + 
-  #geom_label(aes(label = place_of_birth), 
+  labs(title = "Of the 22.1 million <span style = 'color:#18377A;'>Asians</span>  in the US, <span style = 'color:#18377A;'>57.1%</span> are foreign born",x = "", y = "") + 
+  geom_text(aes(label = scales::percent(pct)),
+            color = "#F3F4F6",
+            fontface = "bold",
+            position = position_stack(vjust = 0.5), family = "Karla", size = 5) + 
+  geom_label(aes(x = label, y = c(0,7462176+2000000,16963096+50000),
+                 label = c("U.S. born","Foreign born;\nnaturalized U.S. citizen","Foreign born;\nnot a U.S. citizen"), 
+                 color = fct_reorder(place_of_birth,population)),
+             nudge_x = -0.55,
+             hjust = 0,
+             vjust = 1,
+             fontface = "bold",
+             fill = "#F3F4F6",
+             #box.padding = 0.5,
+             #color = "#F3F4F6",
              #position = position_dodge(0.9),
-            #position = position_stack(vjust = 0.2),
-            #family = "Karla", size = 4) +
-  scale_fill_manual(values = c("#DCB0F2","#F89C74","#8BE0A4")) +
-  #scale_fill_paletteer_d("rcartocolor::Pastel") +
+            family = "Karla", size = 5) +
+  #scale_fill_manual(values = c("#DCB0F2","#F89C74","#8BE0A4")) +
+  scale_x_continuous(expand = expansion(0.15,0),limits=c(-0.2,1.5)) +
+  scale_fill_paletteer_d("jcolors::pal9") +
+  scale_color_paletteer_d("jcolors::pal9") +
   coord_flip() +
   theme_nothing()+
   theme(axis.line.y = element_blank(),
         plot.margin = margin(0),
         axis.text.y = element_blank(),
-        plot.title = element_text("Karla", face = "bold", size = 16, color = "gray20", hjust = 0.16))
+        plot.title = element_markdown("Karla", face = "bold", size = 20, color = "gray50", hjust = 0.22))
 
 entered_plot <- entered %>% 
   ggplot(aes(entered, pct)) +
-  geom_segment( aes(x=entered, xend=entered, y=0, yend=pct), size = 2, show.legend = FALSE, color = "#C9DB74") +
-  geom_point(size = 5, show.legend = FALSE, color = "#C9DB74") +
-  labs(title = "   Most Asians in America have been\nhere for more than 10 years",x = "", y = "") + 
+  geom_segment( aes(x=entered, xend=entered, y=0, yend=pct), size = 2, show.legend = FALSE, color = "#FF7844") +
+  geom_point(size = 5, show.legend = FALSE, color = "#FF7844") +
+  geom_text(aes(label = scales::percent(pct)), 
+            #position = position_stack(vjust = 0.5), 
+            vjust = -1.6,
+            family = "Karla", size = 5) + 
+  labs(title = "Most <span style = 'color:#18377A;'>Asians</span> in America have been here for more than <span style = 'color:#FF7844;'>10 years</span>",x = "", y = "") + 
   scale_x_discrete(labels = c("Entered\nbefore 2000","Entered\n2000 to 2009","Entered\n2010 or later")) +
+  scale_y_continuous(expand = expansion(0,0), limits = c(0,.54)) +
   theme(axis.line.y = element_blank(),
         axis.text.y = element_blank(),
         axis.text.x = element_text("Karla", face = "bold", size = 14),
-        plot.title = element_text("Karla", face = "bold", size = 16, color = "gray20", hjust = 0.16))
+        plot.title = element_markdown("Karla", face = "bold", size = 20, color = "gray50", hjust = 0.3))
 
 pop_plot %>% 
   ggdraw() +
-  draw_plot(pob_plot,.4,.05,.5,.15) +
-  draw_plot(entered_plot,.55,.25,.3,.5) +
-  ggsave("asian_americans.png", device = "png", type = "cairo", width = 17, height = 9, dpi = 300)
+  draw_plot(pob_plot,.40,.045,.57,.2) +
+  draw_plot(entered_plot,.47,.27,.425,.5) +
+  ggsave("asian_americans.png", device = "png", type = "cairo", width = 17, height = 10, dpi = 300)
+
